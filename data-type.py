411809df360b62e47c0d05352c426aba1c260dff
齐文豪 <dawangong@126.com>
2021-06-30 23:01:57 +0800

    learn some basic kownledge

---
+++ data-type.py
@@ -0,0 +1,28 @@
+'''
+Author: your name
+Date: 2021-06-30 22:56:46
+LastEditTime: 2021-06-30 22:58:59
+LastEditors: Please set LastEditors
+Description: In User Settings Edit
+FilePath: /Python/basic.py
+'''
+
+a = 1
+b = "b"
+c = 3
+c +=3
+PI = 3.14
+age = False
+
+if age >= 18:
+    print('adult')
+else:
+    print('teenager')
+
+print(10/3)
+print(10//3)
+print(10%3)
+
+print(None)
+
+print(a, b, c, PI)
---
+++ hello.py
@@ -0,0 +1,11 @@
+'''
+Author: your name
+Date: 2021-06-30 22:46:07
+LastEditTime: 2021-06-30 22:56:30
+LastEditors: Please set LastEditors
+Description: In User Settings Edit
+FilePath: /Python/hello.py
+'''
+
+print ("hello world")
+print('Im \"ok\".')
